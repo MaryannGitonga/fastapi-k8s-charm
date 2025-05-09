704f6e9649a65a5c0e72fe97e1a132572eb37551
MaryannGitonga <naygitonga4@gmail.com>
2025-05-09 14:14:34 +0300

    init project

---
+++ src/charm.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+
+import ops
+import logging
+
+# log messages can be retrieved using juju debug-log
+logger = logging.getLogger(__name__)
+
+class FastAPIDemoCharm(ops.CharmBase):
+    """
+    Charm the service
+    """
+
+    def __init__(self, framework: ops.Framework) -> None:
+        self.pebble_service_name = "fastapi-service"
+
+        super().__init__(framework)
+    
+    def _on_demo_server_pebble_ready(self, event: ops.PebbleReadyEvent) -> None:
+        """
+        Define & start a workload using the Pebble API
+        """
+
+        # get a reference (the container attribute) on the PebbleReadyEvent
+        container = event.workload
+
+        # add initial Pebble config layer using the Pebble API
+        container.add_layer("fastapi_demo", self._pebble_layer, combine=True)
+
+        # make Pebble re-evaluate its plan, ensuring any services are started if enabled
+        container.replan()
+
+        self.unit.status = ops.ActiveStatus()
+    
+    @property
+    def _pebble_layer(self) -> ops.pebble.Layer:
+        """
+        A Pebble layer for the FastAPI demo services.
+        """
+
+        command = ' '.join([
+            'uvicorn',
+            'api_demo_server.app:app',
+            '--host=0.0.0.0',
+            '--port=800',
+        ])
+
+        pebble_layer: ops.pebble.LayerDict = {
+            'summary': 'FastAPI demo service',
+            'description': 'pebble config layer for FastAPI demo server',
+            'services': {
+                self.pebble_service_name: {
+                    'override': 'replace',
+                    'summary': 'fastapi demo',
+                    'command': command,
+                    'startup': 'enabled',
+                }
+            }
+        }
+
+        return ops.pebble.Layer(pebble_layer)
+    
+if __name__ == "__main__":
+    ops.main(FastAPIDemoCharm)
