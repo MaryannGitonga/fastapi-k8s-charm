b98b8473a2029cfdec48d19a2df42788508ee873
MaryannGitonga <naygitonga4@gmail.com>
2025-05-13 13:40:30 +0200

    Add config event hook & tests

--- src/charm.py
+++ src/charm.py
@@ -13,24 +13,49 @@ class FastAPIDemoCharm(ops.CharmBase):
 
     def __init__(self, framework: ops.Framework) -> None:
         super().__init__(framework)
+
         self.pebble_service_name = "fastapi-service"
+        self.container = self.unit.get_container("demo-server")
+
         framework.observe(self.on.demo_server_pebble_ready, self._on_demo_server_pebble_ready)
+        framework.observe(self.on.config_changed, self._on_config_changed)
 
     def _on_demo_server_pebble_ready(self, event: ops.PebbleReadyEvent) -> None:
         """
         Define & start a workload using the Pebble API
         """
 
-        # get a reference (the container attribute) on the PebbleReadyEvent
-        container = event.workload
+        self._update_layer_and_restart()
+    
+    def _on_config_changed(self, event: ops.ConfigChangedEvent) -> None:
+        port = self.config["server-port"]
 
-        # add initial Pebble config layer using the Pebble API
-        container.add_layer("fastapi_demo", self._pebble_layer, combine=True)
+        if port == 22:
+            self.unit.status = ops.BlockedStatus("Invalid port number, port 22 is reserved for SSH")
+            return
+    
+        logger.debug("New application port is requested: %s", port)
+        self._update_layer_and_restart()
+    
+    def _update_layer_and_restart(self) -> None:
+        """
+        Define & start a workload using the Pebble API.
 
-        # make Pebble re-evaluate its plan, ensuring any services are started if enabled
-        container.replan()
+        Need to specify the right entrypoint and env config for specific workload.
+        """
 
-        self.unit.status = ops.ActiveStatus()
+        self.unit.status = ops.MaintenanceStatus('Assembling Pebble layers')
+        try:
+            self.container.add_layer('fastapi_demo', self._pebble_layer, combine=True)
+            logger.info("Added updated layer 'fastapi_demo' to Pebble plan")
+
+            # tell Pebble to incorporate the changes, including restarting the service if required
+            self.container.replan()
+            logger.info(f"Replanned with '{self.pebble_service_name}' service")
+
+            self.unit.status = ops.ActiveStatus()
+        except (ops.pebble.APIError, ops.pebble.ConnectionError):
+            self.unit.status = ops.MaintenanceStatus('Waiting for Pebble in workload container')
 
     @property
     def _pebble_layer(self) -> ops.pebble.Layer:
@@ -42,7 +67,7 @@ class FastAPIDemoCharm(ops.CharmBase):
             'uvicorn',
             'api_demo_server.app:app',
             '--host=0.0.0.0',
-            '--port=8000',
+            f"--port={self.config['server-port']}",
         ])
 
         pebble_layer: ops.pebble.LayerDict = {
